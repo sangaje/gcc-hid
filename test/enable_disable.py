11ae870c7c65e094983395a2a9d66c3e097c6f00
sangaje <qhals8380@gmail.com>
2026-03-12 16:44:48 +0900

    test: Add enable_disable script for managing device notifications and key presses

---
+++ test/enable_disable.py
@@ -0,0 +1,46 @@
+from gcc import GCC, Keyboard
+
+
+if __name__ == "__main__":
+    GCC.RefreshDevices()
+    devices = GCC.GetAllConnectedDevices()
+    print(f"Connected Devcies: {list(devices)}")
+    GCC.EnalbeAllDevicesNotify()
+    print("Press to All")
+    Keyboard.press_key('a')
+    Keyboard.press_key('b')
+    Keyboard.press_key('c')
+
+    GCC.DisalbeAllDevicesNotify()
+    print("Press to No one")
+    Keyboard.press_key('d')
+    Keyboard.press_key('e')
+    Keyboard.press_key('f')
+
+    GCC.EnalbeAllDevicesNotify()
+    print("Press to All")
+    Keyboard.press_key('a')
+    Keyboard.press_key('b')
+    Keyboard.press_key('c')
+
+
+    # if len(devices) != 2:
+    #     print("We need 2 devices")
+
+    GCC.DisalbeDeviceNotify(devices[-1])
+    print(f"Press to only {devices[0]}")
+    Keyboard.press_key('g')
+    Keyboard.press_key('h')
+    Keyboard.press_key('i')
+
+    GCC.EnalbeDeviceNotify(devices[-1])
+    GCC.DisalbeDeviceNotify(devices[0])
+
+    print(f"Press to only {devices[-1]}")
+    Keyboard.press_key('j')
+    Keyboard.press_key('k')
+    Keyboard.press_key('l')
+
+    GCC.EnalbeAllDevicesNotify()
+    Keyboard.send_string("testdone")
+    
